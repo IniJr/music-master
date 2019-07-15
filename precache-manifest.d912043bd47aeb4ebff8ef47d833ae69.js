5e5c93934163b16a21b9e70502fdf84b7786b80b
Antonia Alexis <tonia124356@gmail.com>
2019-07-15 11:59:20 +0100

    Updates

---
+++ precache-manifest.d912043bd47aeb4ebff8ef47d833ae69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad657c240a2aa753063df72b488a1d34",
+    "url": "/DevGemmy/music-master.git/index.html"
+  },
+  {
+    "revision": "ffead48a55c177696763",
+    "url": "/DevGemmy/music-master.git/static/css/main.f2a27027.chunk.css"
+  },
+  {
+    "revision": "6c722ae1d58562715d55",
+    "url": "/DevGemmy/music-master.git/static/js/2.083cabe1.chunk.js"
+  },
+  {
+    "revision": "ffead48a55c177696763",
+    "url": "/DevGemmy/music-master.git/static/js/main.d2de4015.chunk.js"
+  },
+  {
+    "revision": "ee9d5314980df5f6624e",
+    "url": "/DevGemmy/music-master.git/static/js/runtime~main.f87ca92a.js"
+  }
+]);
